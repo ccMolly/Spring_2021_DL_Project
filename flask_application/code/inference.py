9a7e93421a4de9bf500ecbefc7499b1149fe06e3
Mingxi <chenmingxi418@hotmail.com>
2021-05-15 02:20:19 -0400

    add comment

--- flask_application/code/inference.py
+++ flask_application/code/inference.py
@@ -16,12 +16,14 @@ import torchsummary
 from prune import prune_network
 from parameter import build_parser
 
-
+# Main logic of this application
 def inference(img_name, prune_type, model_type):
     print(prune_type)
     print(model_type)
+    # 10 classes of Cifar10 dataset
     classes = ['airplane', 'automobile', 'bird', 'cat','deer','dog','frog','horse','ship','truck']
 
+    # Check if cuda is available
     print("CUDA")
     print(torch.cuda.is_available())
     cuda = False
@@ -35,9 +37,12 @@ def inference(img_name, prune_type, model_type):
                         transforms.ToTensor(),
                         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
 
+    # If user choose "channel" as the prune_type, it means we will use channel-level pruned model to inference this image
     if prune_type == "channel":
+        # If user choose "resnet" as the model_type, it means we will use channel-level pruned ResNet50 to inference this image
         if model_type == "resnet":
             model = models.__dict__["resnet"](dataset="cifar10", depth=50, cfg=[44, 56, 64, 100, 62, 61, 72, 51, 64, 182, 125, 128, 120, 121, 128, 138, 118, 127, 177, 126, 128, 418, 254, 256, 92, 191, 253, 226, 243, 256, 347, 250, 254, 545, 256, 254, 543, 254, 255, 20, 12, 175, 7, 3, 41, 4, 4, 77, 1405])
+        # If user choose "vgg" as the model_type, it means we will use channel-level pruned VGG19 to inference this image
         if model_type == "vgg":
             model = models.__dict__["vgg"](dataset="cifar10", depth=19, cfg=[35, 64, 'M', 127, 128, 'M', 253, 249, 193, 125, 'M', 75, 21, 6, 2, 'M', 8, 9, 13, 67])
 
@@ -48,7 +53,9 @@ def inference(img_name, prune_type, model_type):
 
         model.load_state_dict(checkpoint['state_dict'])
 
+    # If user choose "filter" as the prune_type, it means we will use filter-level pruned model to inference this image
     if prune_type == "filter":
+        # If user choose "resnet" as the model_type, it means we will use filter-level pruned ResNet50 to inference this image
         if model_type == "resnet":
             parser = build_parser()
             args = parser.parse_args(["--vgg", "resnet50", "--prune-flag", "--load-path", "resnet_trained_models/check_point.pth", 
@@ -61,6 +68,7 @@ def inference(img_name, prune_type, model_type):
                 checkpoint = torch.load("resnet_trained_models/check_point_retrain.pth", map_location=torch.device('cpu'))
 
             model.load_state_dict(checkpoint['state_dict'])
+        # If user choose "vgg" as the model_type, it means we will use filter-level pruned VGG19 to inference this image
         if model_type == "vgg":
             parser = build_parser()
             args = parser.parse_args(["--vgg", "vgg19_bn", "--prune-flag", "--load-path", "vgg_trained_models/check_point.pth",
@@ -69,9 +77,12 @@ def inference(img_name, prune_type, model_type):
                                     "46", "256", "256", "256", "256", "256", "256", "256", "256", "--independent-prune-flag"])
             model = prune_network(args, network=None)
 
+    # If user choose "none" as the prune_type, it means we will use original model to inference this image
     if prune_type == "none":
+        # If user choose "resnet" as the model_type, it means we will use original ResNet50 to inference this image
         if model_type == "resnet":
             model = models.__dict__["resnet"](dataset="cifar10", depth=50)
+        # If user choose "vgg" as the model_type, it means we will use original VGG19 to inference this image
         if model_type == "vgg":
             model = models.__dict__["vgg"](dataset="cifar10", depth=19)
 
@@ -87,6 +98,7 @@ def inference(img_name, prune_type, model_type):
 
     model.eval()
     
+    # Open and transform test image
     img = Image.open(img_name).convert('RGB')
     img_t = transform(img)
     print(img_t.shape)
@@ -108,6 +120,7 @@ def inference(img_name, prune_type, model_type):
         ender.record()
         # WAIT FOR GPU SYNC
         torch.cuda.synchronize()
+        # Calculate inference time
         curr_time = starter.elapsed_time(ender)/1000
         pred = output.data.max(1, keepdim=True)[1] # get the index of the max log-probability
 
@@ -117,6 +130,8 @@ def inference(img_name, prune_type, model_type):
         curr_time = time.time()-start
         pred = output.data.max(1, keepdim=True)[1]
 
+    # Apply torchsummary library to show model structure
     s, (total_params, trainable_params) = torchsummary.summary_string(model, (3,32,32))
 
+    # Return predicted class name, inference time and prune_type/model_type selected by user
     return classes[pred], str(curr_time), s, prune_type, model_type
--- flask_application/code/main.py
+++ flask_application/code/main.py
@@ -12,16 +12,20 @@ app = Flask(__name__, static_folder='user_imgs')
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
+        # Receive image sent from user
         image_name = f'{int(time.time())}.jpg'
         image_file = request.files['image']
         image_loc = os.path.join("user_imgs", image_name)
         image_file.save(image_loc)
+        # Receive prune_type sent from user: none / channel / filter
         prune_type = request.form.get('prune_type')
-        
+        # Receive model_type sent from user: vgg / resnet
         model_type = request.form.get('model_type')
 
+        # Call inference method and get back predicted class name, model inference type and prune_type/model_type selected by user
         class_name, inference_time, s, prune_type, model_type = inference(image_loc, prune_type, model_type)
         
+        # Render html to show results
         return render_template('index.html', res=class_name, image_name=image_name, 
             inference_time=inference_time, model_summary=s, prune_type=prune_type, model_type=model_type)
 
